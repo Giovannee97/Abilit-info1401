7592ad6a1f0392b1fccb089273168cf05ebe3f9d
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2022-03-17 10:46:28 +0000

    es1.1

---
+++ src/es1.py
@@ -0,0 +1,11 @@
+class SpPoint:
+  def __init__(self,x,y,z):
+    self.x=x
+    self.y=y
+    self.z=z
+    
+p0=SpPoint(0,0,0)
+
+print("Coordinate punto iniziale:",p0.x,p0.y,p0.z)
+
+print("Inserire coordinate punto p0:")
