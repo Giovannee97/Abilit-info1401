ab2819f206154e6a8f5fc7728a7db745b35c7017
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2022-03-17 11:11:22 +0000

    es1.2

--- src/es1.py
+++ src/es1.py
@@ -1,11 +1,20 @@
 class SpPoint:
   def __init__(self,x,y,z):
-    self.x=x
-    self.y=y
-    self.z=z
+    self.x=float(x)
+    self.y=float(y)
+    self.z=float(z)
     
 p0=SpPoint(0,0,0)
 
+p0.x=input("coordinata x:")
+p0.y=input("coordinata y:")
+p0.z=input("coordinata z:")
+
 print("Coordinate punto iniziale:",p0.x,p0.y,p0.z)
 
-print("Inserire coordinate punto p0:")
+n=integer(input("quanti punti vuoi definire?"))
+
+for i in range(n):
+  p i.x=input("coordinata x:")
+  p i.y=input("coordinata y:")
+  p i.z=input("coordinata z:")
