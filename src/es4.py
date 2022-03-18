871191c4a03454d28039b26faf0786ef9f5aaf0f
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2022-03-18 18:12:09 +0000

    es4.4

--- src/es4.py
+++ src/es4.py
@@ -7,9 +7,12 @@ class CS:
     print("Ho aggiunto la squadra:",self.name,self.score)
   def __del__(self):
     print("Squadra eliminata")
+  def printa(self):
+    print(self.name,self.score)
 
 teams=[]
 nteams=[]
+steams=[]
 
 temp1="sì"
 
@@ -27,8 +30,8 @@ while temp1=="sì":
 
     teams.append(obj) 
     nteams.append(obj.name)
-    print(nteams)
-
+    steams.append(obj.score)
+    
   else:
     pass
 
@@ -43,16 +46,30 @@ while temp2=="sì":
   
   if temp2=="sì":
     namedel=str(input("nome squadra da eliminare:"))
+    
     for i in range(len(nteams)):
       if namedel==nteams[i]:
         del teams[i]
-      else:
-      
   else:
     pass
-    
+
 else:
   pass
-    
-    
+
+temp3="sì"
+
+while temp3=="sì":
   
+  temp2=input(str("Vuoi visualizzare una squadra?"))
+  
+  if temp3=="sì":
+    namesrc=str(input("nome squadra da visualizzare:"))
+    
+    for i in range(len(nteams)):
+      if namesrc==nteams[i]:
+        CS.printa(teams[i])
+  else:
+    pass
+
+else:
+  pass
