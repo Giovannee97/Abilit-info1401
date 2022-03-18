a1c5a1c2bfb012d9c5d1fdcb867a30c45700804c
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2022-03-18 09:40:13 +0000

    es2.1

---
+++ src/es2.py
@@ -0,0 +1,14 @@
+import math
+import numpy
+
+import numpy as np
+coord = np.loadtxt("ex2_data_python_4.txt", dtype='f', delimiter=None)
+
+print("Matrice delle coordinate:")
+print(coord)
+
+x=[]
+
+x.append(((1.0/len(coord))*coord.sum(axis=0)))
+
+print("Le coordinate del baricentro sono:",x[0])
