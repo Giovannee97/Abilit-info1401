e6ef9303f3f7c4937d1413745462c57299dab07c
Giovannee97 <95907567+Giovannee97@users.noreply.github.com>
2022-03-20 21:27:28 +0000

    es6.1

---
+++ src/es6.py
@@ -0,0 +1,10 @@
+plusten=lambda a:a+10
+print(plusten(10))
+
+#La funzione ha come argomento "a" e somma ad esso 10.
+
+animali=["cani","gatti","scoiattoli","alci"]
+f=lambda animale:animale.capitalize()
+print(f(animali[1]))
+
+#La funzione ha come argomento un singolo elemento stringa e riscrive l'elemento con la lettera maiuscola
